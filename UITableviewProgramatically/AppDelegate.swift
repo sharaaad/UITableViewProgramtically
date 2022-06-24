aa0e7ea6d465591133fae186d70a61dd8e4dc151
sharaaad <sharadpun@hotmail.co.uk>
2022-06-24 14:00:03 +0100

    Git commit test

--- UITableviewProgramatically/AppDelegate.swift
+++ UITableviewProgramatically/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+//git revision 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
